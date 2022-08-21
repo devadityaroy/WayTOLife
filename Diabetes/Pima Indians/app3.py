91a5fce3a53390bf68e8c3e164083efe0a7bf31c
Devaditya Roy <95544116+devadityaroy@users.noreply.github.com>
2022-08-21 18:58:54 +0530

    Add files via upload

---
+++ Diabetes/Pima Indians/app3.py
@@ -0,0 +1,69 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
+
+pickle_in = open('LR.pkl', 'rb')
+classifier = pickle.load(pickle_in)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+st.title("Pima Indian Diabetes Disease prediction.")
+
+st.sidebar.header('Diabetes Prediction')
+select = st.sidebar.selectbox('Select Form', ['Form 1'], key='1')
+if not st.sidebar.checkbox("Hide", True, key='1'):
+    st.title('Diabetes Prediction(Only for females above 21years of    Age)')
+    name = st.text_input("Name:")
+    pregnancy = st.number_input("No. of times pregnant:")
+    glucose = st.number_input("Plasma Glucose Concentration :")
+    bp =  st.number_input("Diastolic blood pressure (mm Hg):")
+    skin = st.number_input("Triceps skin fold thickness (mm):")
+    insulin = st.number_input("2-Hour serum insulin (mu U/ml):")
+    bmi = st.number_input("Body mass index (weight in kg/(height in m)^2):")
+    dpf = st.number_input("Diabetes Pedigree Function:")
+    age = st.number_input("Age:")
+submit = st.button('Predict')
+
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://www.heflin.dev/" target="_blank">Heflin Stephen Raj S</a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
+
+
+if submit:
+        prediction = classifier.predict([[pregnancy, glucose, bp, skin, insulin, bmi, dpf, age]])
+        if prediction == 0:
+            st.write('Congratulation',name,'You are not diabetic')
+        else:
+            st.write(name," we are really sorry to say but it seems like you are Diabetic.")
